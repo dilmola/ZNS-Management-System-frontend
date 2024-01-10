e240fba6027b8ecfa442a2440b9cf829819956e2
dilmola <m.aidilmaula@gmail.com>
2024-01-11 05:29:46 +0800

    s

--- src/API/ApiService.js
+++ src/API/ApiService.js
@@ -1,60 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
-const ApiService = {
-  baseURL: "http://127.0.0.1:8000/api",
+let userId; // Variable to store the user ID
 
-  post: async (endpoint, data) => {
-    try {
-      const response = await axios.post(
-        `${ApiService.baseURL}/${endpoint}`,
-        data
-      );
-      return response.data;
-    } catch (error) {
-      throw new Error(`Error fetching data from API: ${error.message}`);
-    }
-  },
-
-  update: async (endpoint, data) => {
-    try {
-      const response = await axios.put(
-        `${ApiService.baseURL}/${endpoint}`,
-        data,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-      console.log("API Response Data:", response.data);
-
-      return response.data;
-    } catch (error) {
-      throw new Error(`Error updating data via API: ${error.message}`);
-    }
-  },
-
-  get: async (endpoint, params) => {
-    try {
-      const response = await axios.get(`${ApiService.baseURL}/${endpoint}`, {
-        params,
-      });
-      console.log("API Response:", response.data);
-      return response.data;
-    } catch (error) {
-      throw new Error(`Error fetching data from API: ${error.message}`);
-    }
-  },
-
-  getUserInfo: async (userId) => {
-    try {
-      const response = await axios.get(`${ApiService.baseURL}/user/${userId}`);
-      return response.data || {};
-    } catch (error) {
-      throw error;
-    }
-  },
+export const setUserId = (id) => {
+  userId = id;
 };
 
-export const getUserInfo = ApiService.getUserInfo;
-export default ApiService;
+export const getUserData = async (userId) => {
+  try {
+    const response = await axios.get(`http://127.0.0.1:8000/api/user/${userId}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
